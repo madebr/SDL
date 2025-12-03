68bd53c98ec955fb0a0bc7fc250bee9d01d85228
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-12-03 23:23:32 +0100

    preprocess input source

--- build-scripts/cake-wrapper.py
+++ build-scripts/cake-wrapper.py
@@ -13,6 +13,9 @@ import tempfile
 DEBUG_WRAPPER = os.getenv("DEBUG_CAKE_WRAPPER") is not None
 CAKE = os.getenv("CAKE") or "cake"
 
+# cake-wrapper.py test: skip cake
+PASSTHROUGH = os.getenv("CAKE_WRAPPER_DISABLE_CAKE") is not None
+
 def ok_to_include(s: str) -> bool:
     assert s.startswith("-I")
     s = s.removeprefix("-I")
@@ -21,80 +24,53 @@ def ok_to_include(s: str) -> bool:
 
 
 def main() -> int:
-    compiler_cmd = []
-    cake_args = []
-    i = 0
-    original_src = None
-    caked_rc = None
-    dep_path = None
-    while i < len(sys.argv):
-        consumed = 1
-        arg = sys.argv[i]
-        if arg.startswith("-I"):
-            if len(arg) > 2:
-                inc_arg = arg
-            else:
-                consumed = 2
-                inc_arg = f"-I{sys.argv[i + 1]}"
-            if ok_to_include(inc_arg):
-                cake_args.append(inc_arg)
-        elif arg.startswith("-isystem"):
-            # cake does not support -isystem
-            if len(arg) > 8:
-                inc_arg = f"-I{arg[8:]}"
-            else:
-                consumed = 2
-                inc_arg = f"-I{sys.argv[i + 1]}"
-            if ok_to_include(inc_arg):
-                cake_args.append(inc_arg)
-        elif arg.startswith("-D"):
-            cake_args.append(arg)
-        elif arg.startswith("-U"):
-            cake_args.append(arg)
-        elif arg.startswith("-MF"):
-            if len(arg) > 3:
-                dep_path = Path(arg[2:])
-            else:
-                consumed = 2
-                dep_path = Path(sys.argv[i + 1])
-        i += consumed
-    cake_args.extend([
-        "-I/usr/lib/gcc/x86_64-redhat-linux/15/include",
-        "-I/usr/local/include",
-        "-I/usr/include",
-        "-target=x86_x64_gcc",
-    ])
-    for arg in sys.argv[1:]:
-        if arg.endswith(".c"):
-            original_src = Path(arg)
-            caked_src = Path(tempfile.gettempdir()) / original_src.with_suffix(".cake.c").name
-            cake_cmd = [CAKE] + cake_args + [f"-I{original_src.parent}", str(original_src), "-o", str(caked_src), "-test-mode"]
-            compiler_cmd.append(f"-I{caked_src.parent}")
+    compiler = sys.argv[1]
+    command = sys.argv[1:]
+    pos_c = command.index("-c")
+    if pos_c >= 0:
+        assert pos_c >= 0
+        command[pos_c + 0] = "-E"
+        input_src = command[pos_c + 1]
+        assert input_src.endswith(".c") or input_src.endswith(".cpp")
+        preproc_src = Path(tempfile.gettempdir()) / Path(input_src).with_suffix(".preproc.c").name
+        pos_o = command.index("-o")
+        assert pos_o >= 0
+        assert pos_o + 1 < len(command)
+        output_file = command[pos_o + 1]
+        command[pos_o + 1] = str(preproc_src)
+        preproc_cmd = command + ["-P"]
+        if DEBUG_WRAPPER:
+            print(f"Running preproc: '{shlex.join(preproc_cmd)}'", file=sys.stderr)
+        rc = subprocess.call(preproc_cmd)
+        if rc:
+            return rc
+
+        if PASSTHROUGH:
+            caked_src = preproc_src
+        else:
+            caked_src = Path(tempfile.gettempdir()) / Path(input_src).with_suffix(".cake.c").name
+            cake_cmd = [CAKE, str(preproc_src), "-o", str(caked_src), "-test-mode"]
             if DEBUG_WRAPPER:
                 print(f"Running cake: '{shlex.join(cake_cmd)}'", file=sys.stderr)
-            result = subprocess.call(cake_cmd)
-            if DEBUG_WRAPPER:
-                print(f"cake exit code: {result}", file=sys.stderr)
-                sys.stderr.flush()
-            if result != 0:
-                print(f"Cake failed: {shlex.join(cake_cmd)}", file=sys.stderr)
-                sys.stderr.flush()
-                return result
-            arg = str(caked_src)
-        compiler_cmd.append(arg)
+            rc = subprocess.call(cake_cmd)
+            if rc:
+                return rc
 
-    if DEBUG_WRAPPER:
-        print(f"Running compiler: '{shlex.join(compiler_cmd)}'", file=sys.stderr)
-        sys.stderr.flush()
-    rc = subprocess.call(compiler_cmd)
-    if dep_path and dep_path.is_file():
-        with dep_path.open("r") as f:
-            original_dep = f.read()
-        new_dep = original_dep.replace(str(caked_src), str(original_src))
-        with dep_path.open("w") as f:
-            f.write(new_dep)
+        caked_src = preproc_src
 
-    return rc
+        command[pos_c + 0] = "-c"
+        command[pos_c + 1] = str(caked_src)
+        command[pos_o + 1] = output_file
+        if DEBUG_WRAPPER:
+            print(f"Running compiler: '{shlex.join(command)}'", file=sys.stderr)
+        rc = subprocess.check_call(command)
+
+        return rc
+    else:
+        if DEBUG_WRAPPER:
+            print(f"Running linker: '{shlex.join(command)}'", file=sys.stderr)
+        rc = subprocess.check_call(command)
+        return c
 
 
 if __name__ == "__main__":
